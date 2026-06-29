d67d867222f11bd204ef3a07f91fc48d48309a2a
GUHAPRIYA B <bguhapriyabvn@gmail.com>
2026-06-29 21:29:38 +0530

    Check the GitHub Actions

---
+++ src/test/java/com/demo/GitHubActionsDemo/HelloTest.java
@@ -0,0 +1,15 @@
+package com.demo.GitHubActionsDemo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloTest {
+
+	@Test
+	void testAddition() {
+		
+		assertEquals(2, 1 + 1);
+	}
+
+}
